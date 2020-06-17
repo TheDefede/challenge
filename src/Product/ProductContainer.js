d9f8b4c836f97d5c4b7ae143cce49b7a059e2ec5
TheDefede <62037106+TheDefede@users.noreply.github.com>
2020-06-17 20:45:09 -0300

    Cambios Variados

--- src/Product/ProductContainer.js
+++ src/Product/ProductContainer.js
@@ -1,11 +1,14 @@
 import React, { useState } from 'react'
 import "./container.css";
 
-const ProductContainer = () =>{
+const ProductContainer = ({nombre, alt, imagen}) =>{
 
     return(
         <div id="product-container">
-            
+            <img src={imagen} alt={alt}/>
+            <div>
+                <p>{nombre}</p>
+            </div>
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import ProductContainer from './Product/ProductContainer';
 
+
 const App = () => {
 
     return(
-        <ProductContainer />
+        <>
+        <ProductContainer nombre="Harina" imagen="https://picsum.photos/200"/>
+        <ProductContainer nombre="Tomate" imagen="https://picsum.photos/200"/>
+        <ProductContainer nombre="Fede" imagen="https://picsum.photos/200"/>
+        </>
     )
 }
 
