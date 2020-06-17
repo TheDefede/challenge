561de400f15b0f1664f63ed03da58dcf21f97c43
TheDefede <62037106+TheDefede@users.noreply.github.com>
2020-06-17 19:04:21 -0300

    Empezando

---
+++ src/Product/ProductContainer.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+import "./container.css";
+
+const ProductContainer = () =>{
+
+    return(
+        <div id="product-container">
+            
+        </div>
+    )
+}
+
+
+export default ProductContainer;
